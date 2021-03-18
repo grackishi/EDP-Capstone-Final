5983cb82c0ce5a9de31879203e09916b5d515a87
Grace <grace.kishimoto@du.edu>
2021-03-17 22:28:35 -0600

    Added Unity Project to Folder
    Added final Unity Project to Git folder

---
+++ Prototype1/Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject arTree;
+    [SerializeField] private Camera arCamera;
+    [SerializeField] ARRaycastManager raycastManager;
+
+    List<ARRaycastHit> hitList = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
+            if(raycastManager.Raycast(ray, hitList))
+            {
+                Pose pose = hitList[0].pose;
+                Instantiate(arTree, pose.position, pose.rotation);
+            }
+        }
+    }
+}
